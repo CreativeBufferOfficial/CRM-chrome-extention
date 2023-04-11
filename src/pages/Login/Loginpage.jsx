97dff9d00f13fb6ae73cb1e9e6a9f3ce7309d0d4
deepakcreativebuffer <deepak@creativebuffer.com>
2023-04-11 22:35:38 +0530

    add actions

--- src/pages/Login/Loginpage.jsx
+++ src/pages/Login/Loginpage.jsx
@@ -1,78 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import classes from './Login.module.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { login } from '../../actions/UserAction';
 
 const Loginpage = () => {
-  const [inputValues, setInputValues] = useState({ email: '', password: '' });
-  const [validation, setValidation] = useState({
-    email: '',
-    password: '',
-  });
+  const [loginEmail, setLoginEmail] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
 
-  const handlerChange = (event) => {
-    const { name, value } = event.target;
-    setInputValues({ ...inputValues, [name]: value });
-  };
-  const submitHandler = (event) => {
-    event.preventDefault();
-    let errors = validation;
+  useEffect(
+    () => {
+      // if (error) {
+      //   alert.error(error);
+      //   dispatch(clearErrors());
+      // }
+      // if (isAuthenticated) {
+      //   Navigate(redirect);
+      // }
+    },
+    [
+      // dispatch, alert, error, isAuthenticated, Navigate, redirect
+    ]
+  );
 
-    // email validation
-    const emailCond =
-      "/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$/";
-    if (!inputValues.email.trim()) {
-      errors.email = 'Email is required';
-    } else if (!inputValues.email.match(emailCond)) {
-      errors.email = 'Please ingress a valid email address';
-    } else {
-      errors.email = '';
-    }
-
-    //password validation
-    const password = inputValues.password;
-    if (!password) {
-      errors.password = 'password is required';
-    } else if (password.length < 6) {
-      errors.password = 'Password must be longer than 6 characters';
-    } else {
-      errors.password = '';
-    }
-    console.log(inputValues);
-    return setValidation(errors);
-    //TODO: // Get Form Data
-
-    // TODO: // Login Api Call
-
-    // TODO: // Set Input  blank
+  const loginSubmit = (e) => {
+    e.preventDefault();
+    dispatch(login(loginEmail, loginPassword));
   };
 
   return (
     <Container className={classes.App}>
       <Row></Row>
       <Row className={classes.main}>
-        <Form onSubmit={submitHandler}>
+        <Form onSubmit={loginSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Control
               className={classes.input_field}
               type="email"
               placeholder="Email"
               name="email"
-              onChange={handlerChange}
+              value={loginEmail}
+              onChange={(e) => setLoginEmail(e.target.value)}
             />
-
-            {validation.email && (
-              <Form.Text className={`text-muted ${classes.muted_text} `}>
-                {validation.email}
-              </Form.Text>
-            )}
-            {/* {validation.email && console.log(validation)} */}
-            {/* <Form.Text className="text-muted">
-              Please Enter your email.
-            </Form.Text> */}
           </Form.Group>
 
           <Form.Group className="mb-1" controlId="formBasicPassword">
@@ -81,7 +53,8 @@ const Loginpage = () => {
               type="password"
               placeholder="Password"
               name="password"
-              onChange={handlerChange}
+              value={loginPassword}
+              onChange={(e) => setLoginPassword(e.target.value)}
             />
           </Form.Group>
           <Form.Group
